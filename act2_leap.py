466f61adab6ff0c5d4bfa803043215bb1d2231a9
25-14841-hash <25-14841@g.batstate-u.edu.ph>
2026-09-04 19:43:44 +0800

    Create act2_leap.py
    Add leap year program

---
+++ act2_leap.py
@@ -0,0 +1,7 @@
+# Name: BASIT, KEN ZYRHON V.
+# Section: BMET - 2101
+# Task 3 - Leap Year Test
+
+year = int(input("Enter a year: "))
+
+print((year % 4 == 0 and year % 100 != 0) or year % 400 == 0)
